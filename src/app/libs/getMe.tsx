22ec3f54ba9831c7a494ba905e51c5120851b4b8
NonRoute <chanon.ratn@gmail.com>
2023-10-29 14:30:04 +0700

    Connect user service via API gateway

--- src/app/libs/getMe.tsx
+++ src/app/libs/getMe.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default async function getMe() {
 	try {
-		const response = await axios.get(`http://localhost:8000/api/users/me`, {
+		const response = await axios.get(`${process.env.NEXT_PUBLIC_USER_SERVICE_ENDPOINT}/api/users/me`, {
 			withCredentials: true
 		})
 		return await response.data.data.user
